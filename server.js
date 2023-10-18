78e825c36803b44b40901349395fd77775c86c39
ippishio <waterlava535@gmail.com>
2023-10-18 22:04:18 +0300

    new artnet library

--- server.js
+++ server.js
@@ -1,30 +1,43 @@
 const WebSocket = require('ws');
+const  dmxlib=require('dmxnet');
+import { element_adresses, ip_adresses } from './const';
 
-var nodes = [
-    require("artnet")({ host: "2.36.176.140" }),
-    require("artnet")({ host: "2.121.14.3" }),
-    require("artnet")({ host: "2.142.165.90" }),
-    require("artnet")({ host: "2.24.14.207" })
-]
 const ws = new WebSocket.Server({ port: 8000 });
-
+var dmxnet = new dmxlib.dmxnet({
+    log: { level: 'info' }, 
+    oem: 0, 
+    esta: 0, 
+    sName: "Mendeleev", 
+    lName: "Mendeleev Table", 
+    hosts: ["127.0.0.1"], 
+    errFunc: function(err) {
+                  this.error(`Do some error handling or throw it: ${err.message}, stack: ${err.stack}`);
+              }.bind(this) 
+  });
 function sendToElement(el_index, iso_index, color) {
-    const node_number = Math.floor(el_index / 32);
-    const channel_number = Math.floor((el_index %32) / 4 );
-    const universe_number = (el_index %32) % 4;
-    if(iso_index != 6) {
-        var colors = Array(28*3);
-        for(var i = 0; i < 28*3; i++) {
-            colors[i] = color[i%3];
+    var sender=dmxnet.newSender({
+        ip: ip_adresses[element_adresses[el_index][0]], 
+        subnet: element_adresses[el_index][1], 
+        universe: element_adresses[el_index][2],
+        net: 0, 
+        port: 6454, 
+        base_refresh_interval: 1000 
+      });
+      if(iso_index < 5) {
+        var cnt = 0;
+        for(var i = iso_index*28; i < (iso_index+1)*28; i++) {
+            sender.prepChannel(i,color[cnt%3]);
+            cnt++;
         }
-        nodes[node_number].set([[universe_number], 28*(iso_index-1)], colors)
-    } else {
-        var colors = Array(30*3);
-        for(var i = 0; i < 30*3; i++) {
-            colors[i] = color[i%3];
+        sender.transmit();
+      } else {
+        var cnt = 0;
+        for(var i = iso_index*30; i < (iso_index+1)*30; i++) {
+            sender.prepChannel(i,color[cnt%3]);
+            cnt++;
         }
-        nodes[node_number].set([[universe_number], 30*(iso_index-1)], colors)
-    }
+        sender.transmit();
+      }
 }
 
 ws.on('connection', onConnect);
